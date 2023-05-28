a283d6a3998376ef5c09fd25deacd40a11ddb528
ferasahmad <firastaf878@gmail.com>
2023-05-28 01:06:15 -0400

    🚧 PROGRESS: worked on poster 1

--- src/components/posters/poster-1/index.jsx
+++ src/components/posters/poster-1/index.jsx
@@ -1,18 +1,20 @@
-import { Container } from "./styles";
+import { Container, TracksContainer, Cover } from "./styles";
 import PosterWrapper from "../../poster-wrapper";
 
 const Poster1 = ({ album }) => {
   console.log(album);
+
+  const returnTracks = (tracks) => {
+    if (!tracks) return;
+
+    return tracks.map((track) => <p key={track.id}>{track.name}• </p>);
+  };
+
   return (
     <PosterWrapper>
       <Container>
-        <h1>{album.name}</h1>
-        <img src={album.image} alt="" />
-        <div>
-          <h2></h2>
-          <hr />
-        </div>
-        <p></p>
+        <Cover src={album.image} alt="" />
+        <TracksContainer>{returnTracks(album.tracks)}</TracksContainer>
         <hr />
         <div>
           <img />
--- src/components/posters/poster-1/styles.js
+++ src/components/posters/poster-1/styles.js
@@ -1,3 +1,46 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+
+  h1 {
+    font-size: 20px;
+  }
+
+  hr {
+    border-style: solid;
+    height: 2px;
+    width: 100%;
+    background-color: white;
+  }
+
+  p {
+    line-height: 20px;
+    margin: 0;
+  }
+`;
+
+export const Cover = styled.img`
+  width: 300px;
+  height: 300px;
+`;
+
+export const TracksContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const ArtistNameContainer = styled.div`
+  background-color: #1db954;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+
+  h2 {
+    margin: 0;
+  }
+`;
