4df20f8f9a5985e3f3153b1fba63f166e8f565ef
ferasahmad <firastaf878@gmail.com>
2023-05-28 00:14:31 -0400

    🚧 PROGRESS: poster 1

--- src/components/posters/poster-1/index.jsx
+++ src/components/posters/poster-1/index.jsx
@@ -1,8 +1,27 @@
 import { Container } from "./styles";
 import PosterWrapper from "../../poster-wrapper";
 
-const Poster1 = () => {
-  return <PosterWrapper>POSTER 2</PosterWrapper>;
+const Poster1 = ({ album }) => {
+  console.log(album);
+  return (
+    <PosterWrapper>
+      <Container>
+        <h1>{album.name}</h1>
+        <img src={album.image} alt="" />
+        <div>
+          <h2></h2>
+          <hr />
+        </div>
+        <p></p>
+        <hr />
+        <div>
+          <img />
+          <img />
+          <img />
+        </div>
+      </Container>
+    </PosterWrapper>
+  );
 };
 
 export default Poster1;
